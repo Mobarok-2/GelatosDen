89731ff56ee21f4c60c53b56d4b1731cb2f363a7
MHSiam1 <2223011084@uttarauniversity.edu.bd>
2024-05-04 22:24:19 -0400

    Food Item Updated

--- app/Http/Controllers/Food/FoodController.php
+++ app/Http/Controllers/Food/FoodController.php
@@ -4,8 +4,80 @@ namespace App\Http\Controllers\Food;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+Use App\Models\Food\Food;
 class FoodController extends Controller
 {
-    //
+    
+
+    // public function index() {
+
+
+    // }
+
+
+    // public function index()
+    // {
+    //     return view('food.index');
+    // }
+
+    // public function create()
+    // {
+    //     return view('food.create');
+    // }
+
+    // public function store(Request $request)
+    // {
+    //     $request->validate([
+    //         'name' => 'required',
+    //         'price' => 'required',
+    //         'category' => 'required',
+    //         'description' => 'required',
+    //         'image' => 'required',
+    //     ]);
+
+    //     $food = new Food();
+    //     $food->name = $request->name;
+    //     $food->price = $request->price;
+    //     $food->category = $request->category;
+    //     $food->description = $request->description;
+    //     $food->image = $request->image;
+    //     $food->save();
+
+    //     return redirect()->route('food.index')
+    //         ->with('success', 'Food created successfully.');
+    // }
+
+    // public function show(Food $food)
+    // {
+    //     return view('food.show', compact('food'));
+    // }
+
+    // public function edit(Food $food)
+    // {
+    //     return view('food.edit', compact('food'));
+    // }
+
+    // public function update(Request $request, Food $food)
+    // {
+    //     $request->validate([
+    //         'name' => 'required',
+    //         'price' => 'required',
+    //         'category' => 'required',
+    //         'description' => 'required',
+    //         'image' => 'required',
+    //     ]);
+
+    //     $food->update($request->all());
+
+    //     return redirect()->route('food.index')
+    //         ->with('success', 'Food updated successfully');
+    // }
+
+    // public function destroy(Food $food)
+    // {
+    //     $food->delete();
+
+    //     return redirect()->route('food.index')
+    //         ->with('success', 'Food deleted successfully');
+    // }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+Use App\Models\Food\Food;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $brownieItems = Food::select()->take(4)->where('category', 'brownie')->get();
+        
+        return view('home', compact('brownieItems'));
     }
 }
